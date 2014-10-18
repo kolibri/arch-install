4cdb4e739ae285ff6dbca381d84f33c44d5ed8f7
ko.libri <entengelb@gmail.com>
2014-10-19 01:59:08 +0200

    fix

--- install.sh
+++ install.sh
@@ -7,11 +7,13 @@ echo " \\\\(^)> INSTALL ARCH START <(^)//"
 
 echo '==> bootstrapping the base installation'
 #pacstrap /mnt base base-devel wpa_supplicant
-/usr/bin/pacstrap ${TARGET_DIR} base # base-devel
+/usr/bin/pacstrap ${TARGET_DIR} base base-devel
 
 echo '==> generating the filesystem table'
 /usr/bin/genfstab -p ${TARGET_DIR} >> "${TARGET_DIR}/etc/fstab"
 
-echo "next: arch-chroot ${TARGET_DIR} ${CONFIG_SCRIPT}"
+cp ${CONFIG_SCRIPT} {TARGET_DIR}/root/${CONFIG_SCRIPT}
+
+echo "next: arch-chroot ${TARGET_DIR}"
 echo "next: unmount ${TARGET_DIR}"
 echo "next: systemctl reboot"
