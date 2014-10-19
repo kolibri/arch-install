001821753cda56741907d739cc6cd58d999840c2
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2014-10-19 21:11:26 +0200

    fix

--- dl_scripts.sh
+++ dl_scripts.sh
@@ -5,6 +5,9 @@ curl -O https://raw.githubusercontent.com/kolibri/foo/master/init_utils.sh
 curl -O https://raw.githubusercontent.com/kolibri/foo/master/install.sh
 curl -O https://raw.githubusercontent.com/kolibri/foo/master/prepare-hdd.sh
 
+curl -O https://raw.githubusercontent.com/kolibri/foo/master/arch.conf
+curl -O https://raw.githubusercontent.com/kolibri/foo/master/loader.conf
+
 chmod +x arch-config.sh
 chmod +x init_utils.sh
 chmod +x install.sh
